f0fcf76e1b250503c43bee0a257ea4fcd5833d13
Kamran Mmmdv <amorfrap@gmail.com>
2019-06-14 23:02:17 +0400

    added city selection feature and a glassy smooth design

--- weather/weather_app/views.py
+++ weather/weather_app/views.py
@@ -1,13 +1,20 @@
 import requests
 from django.shortcuts import render
 from .models import City
+from .forms import CityForm
 
 # Create your views here.
 
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=e9e7b90c2e9315e6370b53013e324984&units=metric'
-    city = 'Baku'
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
     cities = City.objects.all()
     weather_data = []
 
@@ -19,12 +26,11 @@ def index(request):
             'temperature': response['main']['temp'],
             'description': response['weather'][0]['description'],
             'icon': response['weather'][0]['icon']
-
         }
 
         weather_data.append(city_weather)
 
-    context = {'weather_data': weather_data}
+    context = {'weather_data': weather_data, 'form': form}
     return render(request, 'weather/index.html', context)
 
 
